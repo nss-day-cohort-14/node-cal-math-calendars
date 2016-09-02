01a314f3868cf2ba23a9dcff031f86ab55362707
Brian Doane <brianmdoane@gmail.com>
2016-09-01 23:34:47 -0500

    added args to chain function describe, isNumber test to makeEnds

--- test/chain.js
+++ test/chain.js
@@ -1,22 +1,26 @@
 'use strict'
 
-const { assert: { isFunction, isString } } = require('chai')
+const { assert: { isFunction, isString, isNumber } } = require('chai')
 
 const { getLeftover, makeEnds, addPad } = require('../lib/chain')
 
 describe('chain', () => {
-	describe('getLeftover', () => {
+	describe('getLeftover(month)', () => {
 		it('should be a function', () => {
 			isFunction(getLeftover)
 		})
-		it('should take one argument', () => {
+		it('should take one argument which is a string', () => {
 			let month = 'August'
 			isString(month)
 		})
 	})	
-	describe('makeEnds', () => {
+	describe('makeEnds(leftOver)', () => {
 		it('should be a function', () => {
 			isFunction(makeEnds)
+		})
+		it('should take one argument which is a numeral', () => {
+			let leftOver = 6
+			isNumber(leftOver)
 		})
 	})
 	describe('addPad', () => {
